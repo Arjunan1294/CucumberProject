9d1e886ec387897229323c20c1ad42b5b8150ad8
Arjunan <g.arjun94@gmail.com>
2021-06-28 08:41:55 +0530

    first

---
+++ test-output-thread/report.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "4f46c60c-a054-4af1-8a61-08afac8923c6",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1624848808791,
+    "group": 16,
+    "content": "",
+    "tags": "@login,@regression,@skip,",
+    "end": 1624848815658,
+    "className": "skipped"
+  },
+  {
+    "id": "c7415926-ff83-4f5a-8381-67723a68970a",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1624848808694,
+    "group": 15,
+    "content": "",
+    "tags": "@login,@smoke,",
+    "end": 1624848825791,
+    "className": "failed"
+  },
+  {
+    "id": "99aa538f-7c45-4dc9-8ef6-e3a93679d31e",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1624848808792,
+    "group": 14,
+    "content": "",
+    "tags": "@login,@smoke,",
+    "end": 1624848825760,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  }
+]);
+});
